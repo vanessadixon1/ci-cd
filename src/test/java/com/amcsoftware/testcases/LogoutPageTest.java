03ad7396ca15c9c06dc348dff94773e61841156f
vanessadixon1 <mclendonfamily88@gmail.com>
2026-06-14 11:20:08 -0400

    testing pull request

---
+++ src/test/java/com/amcsoftware/testcases/LogoutPageTest.java
@@ -0,0 +1,14 @@
+package com.amcsoftware.testcases;
+
+import com.amcsoftware.base.BaseTest;
+import org.testng.annotations.Test;
+
+
+public class LogoutPageTest extends BaseTest {
+
+    @Test(groups = {"Smoke"})
+    public void runTest() {
+        System.out.println("Running LogoutPageTest");
+    }
+
+}
